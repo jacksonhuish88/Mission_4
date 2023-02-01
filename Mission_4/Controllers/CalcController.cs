4da564dacef739d6f98e0655acd8df8987b9fb4f
Jackson Huish <jacksonhuish88@gmail.com>
2023-02-01 09:16:58 -0700

    Mission05_Vid05

---
+++ Mission_4/Controllers/CalcController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
