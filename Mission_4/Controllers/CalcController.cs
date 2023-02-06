48c00ce1fdaabf357c0bceb929b0a622eec59844
Jackson Huish <jacksonhuish88@gmail.com>
2023-02-06 09:15:23 -0700

    css / validation update

--- Mission_4/Controllers/CalcController.cs
+++ Mission_4/Controllers/CalcController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mission_4.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,17 @@ namespace Mission_4.Controllers
     public class CalcController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(CalcModel model)
         {
             return View();
         }
---
+++ Mission_4/Models/CalcModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4.Models
+{
+    public class CalcModel
+    {
+        // Builds Getter & Setter
+        [Required]
+        [Range(1, 100)]
+        public int assignments { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int groupProjects { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int quizzes { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int midterm  { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int final { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public int intex { get; set; }
+
+    }
+}
